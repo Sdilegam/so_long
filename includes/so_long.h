9dc78c676155ede1daf8a48dfe4a7dff30c914ce
sdi-lega <sdi-lega@student.s19.be>
2022-04-12 15:50:55 +0200

    HELP

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,18 +6,18 @@
 /*   By: sdi-lega <sdi-lega@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 12:05:43 by sdi-lega          #+#    #+#             */
-/*   Updated: 2022/04/12 04:47:17 by sdi-lega         ###   ########.fr       */
+/*   Updated: 2022/04/12 15:40:41 by sdi-lega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
+# define BIT 16
 
 # include "get_next_line.h"
 # include "mlx.h"
 # include <stdlib.h>
 # include <fcntl.h>
-
 
 typedef struct s_img {
 	void	*img;
@@ -32,23 +32,24 @@ typedef struct s_map {
 }	t_map;
 typedef struct s_coord
 {
-	int x;
-	int y;
-	int e_x;
-	int e_y;
+	int	x;
+	int	y;
+	int	e_x;
+	int	e_y;
 }	t_coord;
 
-
-typedef struct	s_game {
+typedef struct s_game
+{
+	t_coord	coord;
+	t_map	map;
+	t_img	chest[4];
+	t_img	sprite[5];
+	t_img	chara[4][4];
 	void	*m_ptr;
 	void	*m_win;
-	t_map	map;
-	t_img	sprite[6];
-	t_img	chara[4][4];
 	int		moves;
 	int		facing;
-	int		chest;
-	t_coord	coord;
+	int		goal;
 	int		frame;
-}				t_game;
+}	t_game;
 #endif
--- sources/mandatory/so_long.c
+++ sources/mandatory/so_long.c
@@ -6,28 +6,27 @@
 /*   By: sdi-lega <sdi-lega@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 16:55:40 by sdi-lega          #+#    #+#             */
-/*   Updated: 2022/04/12 05:46:42 by sdi-lega         ###   ########.fr       */
+/*   Updated: 2022/04/12 15:50:33 by sdi-lega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "so_long.h"
 
 void	error(int error_id)
 {
 	if (error_id == 1)
-		write (2, "Error\nThe map given isn't rectangular.\n", 39);
+		write(2, "Error\nThe map given isn't rectangular.\n", 39);
 	exit(1);
 }
 
-int	get_len(char* string)
+int	get_len(char *string)
 {
 	int	index;
 
 	index = 0;
 	while (string[index] != '\n' && string[index])
 		index++;
-	return(index);
+	return (index);
 }
 
 t_map	get_map_size(char *path)
@@ -58,11 +57,11 @@ t_map	get_map_size(char *path)
 
 t_map	read_map(char *path)
 {
-	t_map map;
-	int	index;
-	int	file_des;
+	t_map	map;
+	int		index;
+	int		file_des;
 
-	index= -1;
+	index = -1;
 	map = get_map_size(path);
 	file_des = open(path, O_RDONLY);
 	while (++index < map.h)
@@ -70,104 +69,137 @@ t_map	read_map(char *path)
 	return (map);
 }
 
-void put_img(t_game *g, int x, int y)
+void	put_img(t_game *g, int x, int y)
 {
-	t_img img;
-	int (*put)(void *, void *, void *, int, int) = mlx_put_image_to_window;
-	
+	t_img	img;
+	int		pixel_x;
+	int		pixel_y;
+	int		(*put)(void *, void *, void *, int, int);
+
+	pixel_x = (x + 1) * BIT;
+	pixel_y = (y + 1) * BIT;
+	put = mlx_put_image_to_window;
 	if (g->map.map[y][x] == '1')
 		img = g->sprite[0];
 	else
 	{
 		img = g->sprite[1];
-		put(g->m_ptr, g->m_win, img.img, (x + 1) * 16 - img.w, (y + 1) * 16 - img.h);
+		put(g->m_ptr, g->m_win, img.img, pixel_x - img.w, pixel_y - img.h);
 		if (g->map.map[y][x] == 'C')
-		{
-			(g->chest)++;
-			img = g->sprite[3];
-		}
+			img = g->chest[g->frame];
 		else if (g->map.map[y][x] == 'P')
-		{
 			img = g->chara[g->facing][g->frame];
-			g->coord.x = x;
-			g->coord.y = y;
-		}
 		else if (g->map.map[y][x] == 'E')
-		{
-			img = g->sprite[4];
-			g->coord.e_x = x;
-			g->coord.e_y = y;
-		}
+			img = g->sprite[3];
 	}
-	put(g->m_ptr, g->m_win, img.img, (x + 1) * 16 - img.w, (y + 1) * 16 - img.h);
+	put(g->m_ptr, g->m_win, img.img, pixel_x - img.w, pixel_y - img.h);
 }
 
 void	map_render(t_game *g)
 {
-	int		index[2];
+	int	index1;
+	int	index2;
 
-	index[1] = -1;
-	while (++index[1] < g->map.h)
+	index2 = -1;
+	while (++index2 < g->map.h)
 	{
-		index[0] = -1;
-		while (++index[0] < g->map.w)
-			put_img(g, index[0], index[1]);
+		index1 = -1;
+		while (++index1 < g->map.w)
+		{
+			if (g->map.map[index2][index1] == 'C')
+				g->goal++;
+			else if (g->map.map[index2][index1] == 'P')
+			{
+				g->coord.x = index1;
+				g->coord.y = index2;
+			}
+			else if (g->map.map[index2][index1] == 'E')
+			{
+				g->coord.e_x = index1;
+				g->coord.e_y = index2;
+			}
+			put_img(g, index1, index2);
+		}
+	}
+}
+
+void	update(t_game *g)
+{
+	int		index1;
+	int		index2;
+
+	index2 = -1;
+	while (++index2 < g->map.h)
+	{
+		index1 = -1;
+		while (++index1 < g->map.w)
+		{
+			if (g->map.map[index2][index1] == 'C')
+				put_img(g, index1, index2);
+		}
 	}
 }
 
 void loop_handler(t_game *g)
 {
-	static int	index = -1;
-	static int	index2 = 0;
-	if (++index2 == 2000)
+	static int index = -1;
+	static int index2 = 0;
+	static int index3 = -1;
+
+	if (++index2 == 1000)
 	{
 		if (++index <= 3)
 			g->frame = index;
-		else 
+		else
 			index = -1;
-		index2 = 0;
 		put_img(g, g->coord.x, g->coord.y);
-	}	
-	if (g->chest == 0)
+	}
+	if (index2 == 2000)
 	{
-		g->chest = -1;
-		g->sprite[4] = g->sprite[5];
+		if (++index3 <= 3)
+			g->frame = index3;
+		else
+			index3 = -1;
+		update(g);
+		index2 = 0;
+	}
+	if (g->goal == 0)
+	{
+		g->goal = -1;
+		g->sprite[3] = g->sprite[4];
 		put_img(g, g->coord.e_x, g->coord.e_y);
 	}
 }
 
-int move_character(t_game *g, int x, int y)
+int	move_character(t_game *g, int x, int y)
 {
 	int	pre_x;
 	int	pre_y;
-	
+
 	pre_x = g->coord.x;
 	pre_y = g->coord.y;
-	// printf("%d\n", g->frame);
-
 	if (g->map.map[y][x] != '1' && g->map.map[y][x] != 'E')
 	{
 		printf("You have done %d moves.\n", g->moves++);
-		mlx_string_put(g->m_ptr, g->m_win,16, 30, 0, "test");
 		if (g->map.map[y][x] == 'C')
-			g->chest--;
+			g->goal--;
 		g->map.map[pre_y][pre_x] = '0';
 		put_img(g, pre_x, pre_y);
 		g->map.map[y][x] = 'P';
+		g->coord.x = x;
+		g->coord.y = y;
 		put_img(g, x, y);
 	}
-	if (g->map.map[y][x] == 'E' && g->chest <= 0)
+	if (g->map.map[y][x] == 'E' && g->goal <= 0)
 	{
 		printf("the end");
 		exit(0);
 	}
 }
 
-
-
-int handle_keys(int key, void *game)
+int	handle_keys(int key, void *game)
 {
-	t_game *g;
+	t_game	*g;
 
 	g = (t_game *)game;
 	if (key == 123)
@@ -194,10 +226,11 @@ int handle_keys(int key, void *game)
 
 void	chara_image(t_game *g)
 {
-	t_img *img;
-	
+	t_img	*img;
+	void	*(*f)();
+
 	img = g->chara[0];
-	void *(*f)() = mlx_xpm_file_to_image;
+	f = mlx_xpm_file_to_image;
 	img[0].img = f(g->m_ptr, "Sprites/u1.xpm", &(img[0].w), &(img[0].h));
 	img[1].img = f(g->m_ptr, "Sprites/u2.xpm", &(img[1].w), &(img[1].h));
 	img[2].img = f(g->m_ptr, "Sprites/u3.xpm", &(img[2].w), &(img[2].h));
@@ -219,30 +252,37 @@ void	chara_image(t_game *g)
 	img[3].img = f(g->m_ptr, "Sprites/l4.xpm", &(img[3].w), &(img[3].h));
 }
 
-void	environment_image(t_game *g)
+void environment_image(t_game *g)
 {
-	t_img *img;
-	
+	t_img	*img;
+	void	*(*f)();
+
+	img = g->chest;
+	f = mlx_xpm_file_to_image;
+	img[0].img = f(g->m_ptr, "Sprites/chest1.xpm", &(img[0].w), &(img[0].h));
+	img[1].img = f(g->m_ptr, "Sprites/chest2.xpm", &(img[1].w), &(img[1].h));
+	img[2].img = f(g->m_ptr, "Sprites/chest3.xpm", &(img[2].w), &(img[2].h));
+	img[3].img = f(g->m_ptr, "Sprites/chest4.xpm", &(img[3].w), &(img[3].h));
 	img = g->sprite;
-	void *(*f)() = mlx_xpm_file_to_image;
 	img[0].img = f(g->m_ptr, "Sprites/wall.xpm", &(img[0].w), &(img[0].h));
 	img[1].img = f(g->m_ptr, "Sprites/floor.xpm", &(img[1].w), &(img[1].h));
 	img[2].img = f(g->m_ptr, "Sprites/enemy.xpm", &(img[2].w), &(img[2].h));
-	img[3].img = f(g->m_ptr, "Sprites/chest.xpm", &(img[3].w), &(img[3].h));
-	img[4].img = f(g->m_ptr, "Sprites/c_end.xpm", &(img[4].w), &(img[4].h));
-	img[5].img = f(g->m_ptr, "Sprites/o_end.xpm", &(img[5].w), &(img[5].h));
+	img[3].img = f(g->m_ptr, "Sprites/c_end.xpm", &(img[3].w), &(img[3].h));
+	img[4].img = f(g->m_ptr, "Sprites/o_end.xpm", &(img[4].w), &(img[4].h));
 }
 
 void	init_game(t_game *g, char *param)
 {
-	void *(*f)() = mlx_xpm_file_to_image;
-	g->chest = 0;
+	void	*(*f)();
+
+	f = mlx_xpm_file_to_image;
+	g->goal = 0;
 	g->moves = 0;
-	g->frame = -1;
+	g->frame = 0;
 	g->facing = 0;
 	g->m_ptr = mlx_init();
 	g->map = read_map(param);
-	g->m_win =mlx_new_window(g->m_ptr, g->map.w * 16, g->map.h * 16, "So_long");
+	g->m_win = mlx_new_window(g->m_ptr, g->map.w * BIT, g->map.h * BIT, "S_l");
 	environment_image(g);
 	chara_image(g);
 	map_render(g);
@@ -250,10 +290,12 @@ void	init_game(t_game *g, char *param)
 
 int	main(int argc, char *argv[])
 {
-	t_game game;
+	t_game	game;
+
 	init_game(&game, argv[1]);
 	mlx_key_hook(game.m_win, handle_keys, &game);
 	mlx_loop_hook(game.m_ptr, loop_handler, &game);
+	mlx_do_key_autorepeaton(game.m_ptr);
 	mlx_loop(game.m_ptr);
 	return (0);
 }
